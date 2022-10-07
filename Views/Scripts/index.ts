1c0ec825c09ddcffe2c1ab04a6a4df179ffa962e
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2022-10-07 12:23:44 -0500

    Cocos del frontend terminado

--- Views/Scripts/index.ts
+++ Views/Scripts/index.ts
@@ -1,3 +1,3 @@
 document.getElementById("AlbumManagementButton")?.addEventListener("click", () => window.location.href = "./Pages/Albums.html" );
-document.getElementById("GenreManagementButton")?.addEventListener("click", () => window.location.href = "./Pages/Artist.html" );
-document.getElementById("ArtistManagementButton")?.addEventListener("click", () => window.location.href = "./Pages/Genre.html");
+document.getElementById("GenreManagementButton")?.addEventListener("click", () => window.location.href = "./Pages/Genre.html" );
+document.getElementById("ArtistManagementButton")?.addEventListener("click", () => window.location.href = "./Pages/Artist.html");
